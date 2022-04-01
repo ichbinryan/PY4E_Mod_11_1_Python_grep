ca81f68b8d9dd23ecf5ad14115332a263efeabcd
Ryan Read <Ryan.Read@veteransunited.com>
2022-03-31 22:02:02 -0500

    Add starter code
    Add grep.py

---
+++ grep.py
@@ -0,0 +1,13 @@
+import re
+import string
+import sys
+
+
+def findpatterninfile(filename):
+    regular_expression = input("Enter a regular expression: ")
+
+if __name__ == '__main__':
+    # this is called a main method
+    # This is another way of telling python THIS IS WHERE YOU SHOULD START RUNNING
+    # When this is included, python will begin with the code in this block first
+    findpatterninfile(".\mbox-long.txt")
