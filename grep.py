8f3546b03abc45e1810edf3b29fcf1421cfd1a1f
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2023-01-18 15:54:56 -0600

    Update grep.py

--- grep.py
+++ grep.py
@@ -1,13 +1,8 @@
 import re
-import string
-import sys
-
 
 def countpatterninfile():
     regular_expression = input("Enter a regular expression: ")
 
+    
 if __name__ == '__main__':
-    # this is called a main method
-    # This is another way of telling python THIS IS WHERE YOU SHOULD START RUNNING
-    # When this is included, python will begin with the code in this block first
-    countpatterninfile()
+    count_pattern_in_file()
